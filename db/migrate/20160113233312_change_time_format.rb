19c1bf972ea1e9e28796e965273c3a823a692c97
ronanlopes <lopesronanufsj@gmail.com>
2016-01-13 23:11:37 -0200

    Grade de Horários

--- db/migrate/20160113233312_change_time_format.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTimeFormat < ActiveRecord::Migration
-  def up
-    change_column :grade_horarios, :hora, :string
-  end
-
-  def down
-    change_column :grade_horarios, :hora, :time
-  end
-end
